e3d60b9258dc3db32254ab2df752a5a6de6c153e
thokjo12 <thokjo12@hotmail.com>
2021-03-13 14:59:27 +0100

    continued work on reindex, needs something like indexmap to be viable

--- src/lib.rs
+++ src/lib.rs
@@ -114,61 +114,63 @@ impl<T> Model<T> where T: Document + Debug + Clone {
             .sum::<f64>()
     }
 
-    // pub fn update_index(&mut self) { // TODO refactor and extract processing into separate functions for reuse in construct and update_index.
-    //     if self.queued_for_indexing.is_empty() {
-    //         println!("No documents queued");
-    //         return;
-    //     }
-    //
-    //     let capture = &self.capture;
-    //     let reg = &self.processing_regex;
-    //
-    //     self.documents.extend(self.queued_for_indexing.clone());
-    //     let doc_count = self.queued_for_indexing.len();
-    //     let processed = self.queued_for_indexing
-    //         .drain(..)
-    //         .map(|doc| Model::<T>::preprocess(&doc, capture, reg))
-    //         .collect::<Vec<_>>();
-    //
-    //     let tmp_dict = processed.clone()
-    //         .drain(..)
-    //         .flatten()
-    //         .collect::<HashSet<_>>();
-    //
-    //     // find intersection of tmp dict and self.dict and remove those.
-    //
-    //     self.dictionary.extend(tmp_dict);
-    //
-    //     // let vector_length = tmp_dict.len();
-    //
-    //     let mut document_frequency = vec![0; vector_length];
-    //     let mut term_frequencies: Vec<Vec<usize>> = iter::repeat_with(|| vec![0; vector_length])
-    //         .take(num_docs)
-    //         .collect();
-    //
-    //
-    //     self.index = self.dictionary
-    //         .clone()
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, item)| { (item.clone(), i) })
-    //         .collect::<HashMap<_, _>>();
-    //
-    //     for i in 0..doc_count{
-    //         for term in processed[i].iter(){
-    //             let term_index = self.index.get(term).unwrap().clone();
-    //             if term_frequencies[i][term_index] == 0{
-    //                 document_frequency[term_index] += 1;
-    //             }
-    //             term_frequencies[i][term_index] += 1;
-    //         }
-    //     }
-    //
-    //
-    //     // reconstruct the model.
-    //     //extend self vars with the above.
-    //
-    // }
+    pub fn update_index(&mut self) { // TODO refactor and extract processing into separate functions for reuse in construct and update_index.
+        if self.queued_for_indexing.is_empty() {
+            println!("No documents queued");
+            return;
+        }
+
+        let capture = &self.capture;
+        let reg = &self.processing_regex;
+        let num_docs = self.queued_for_indexing.len();
+
+        self.documents.extend(self.queued_for_indexing.clone());
+        let doc_count = self.queued_for_indexing.len();
+        let processed = self.queued_for_indexing
+            .drain(..)
+            .map(|doc| Model::<T>::preprocess(&doc, capture, reg))
+            .collect::<Vec<_>>();
+
+        let tmp_dict = processed.clone()
+            .drain(..)
+            .flatten()
+            .collect::<HashSet<_>>();
+
+
+        // TODO swap dict, index to use indexmap: https://docs.rs/indexmap/1.6.2/indexmap/
+        self.dictionary.extend(tmp_dict);
+
+        let vector_length = self.dictionary.len();
+
+        let mut document_frequency = vec![0; vector_length];
+        let mut term_frequencies: Vec<Vec<usize>> = iter::repeat_with(|| vec![0; vector_length])
+            .take(num_docs)
+            .collect();
+
+
+        self.index = self.dictionary
+            .clone()
+            .iter()
+            .enumerate()
+            .map(|(i, item)| { (item.clone(), i) })
+            .collect::<HashMap<_, _>>();
+
+        for i in 0..doc_count{
+            for term in processed[i].iter(){
+                let term_index = self.index.get(term).unwrap().clone();
+                if term_frequencies[i][term_index] == 0{
+                    document_frequency[term_index] += 1;
+                }
+                term_frequencies[i][term_index] += 1;
+            }
+        }
+
+        self.document_frequency.iter().enumerate().map(|(i,df)| document_frequency[i]+df).collect::<Vec<_>>();
+
+        // reconstruct the model.
+        //extend self vars with the above.
+
+    }
 
     pub fn calculate_document_weights(&mut self) {
         self.document_weights = self.term_frequencies.iter().map(|document| {
